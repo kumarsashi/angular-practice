5f9db1de5c14d12a1db267e68cac9abd0712d582
sashi ram <sashi.suraj7@gmail.com>
2018-10-24 09:22:57 +0530

    first commit

---
+++ src/app/mainnav/mainnav.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-mainnav',
+  templateUrl: './mainnav.component.html',
+  styleUrls: ['./mainnav.component.css'],
+})
+export class MainnavComponent {
+
+  public applicationTitle: string;
+  public username: string;
+
+
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.applicationTitle = "Keep Note";
+    this.username = "Stackroute";
+  }
+
+  updateUserName($event){
+    this.username = $event.target.value;
+  }
+
+}
